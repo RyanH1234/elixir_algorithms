e8274d1925431c67a7e5b38a0fb4c5a2a4afff0d
Ryan <hodgsonr1234@gmail.com>
2020-07-17 16:39:35 +0800

    (maximum subarray) fixed maximum subarray problem

--- lib/maximum_subarray.ex
+++ lib/maximum_subarray.ex
@@ -1,31 +1,36 @@
 defmodule MaximumSubarray do
 
-  def find_max_subarray([], _acc, max_sum, index), do: { index, max_sum }
-  def find_max_subarray(list, acc, max_sum, index) do
-    [head | tail] = list
-    updated_acc = head + acc
-
+  def find_max_subarray([], _acc, max_sum, max_index), do: { max_index, max_sum }
+  def find_max_subarray(list, acc, max_sum, max_index) do
+    [{val, index} | tail] = list
+    updated_acc = val + acc
     case updated_acc > max_sum do
       true ->
-        find_max_subarray(tail, updated_acc, head + acc, index + 1)
+        find_max_subarray(tail, updated_acc, updated_acc, index)
       _ ->
-        find_max_subarray(tail, updated_acc, max_sum, index)
+        find_max_subarray(tail, updated_acc, max_sum, max_index)
     end
   end
 
-  def find_max_crossing_subarray(list) do
-    midpoint = round(length(list)/2)
-    {lhs, rhs} = Enum.split(list, midpoint)
+  def do_search(list) do
+    list_i = Enum.with_index(list)
+    midpoint = round(length(list_i)/2)
+    {lhs, rhs} = Enum.split(list_i, midpoint)
 
-    {lhs_index, lhs_sum} =
-      lhs
-      |> Enum.reverse()
-      |> find_max_subarray(0, 0, 0)
-
-    lhs_index = length(lhs) - lhs_index
-
+    {lhs_index, lhs_sum} = find_max_subarray(lhs, 0, 0, 0)
     {rhs_index, rhs_sum} = find_max_subarray(rhs, 0, 0, 0)
 
-    {lhs_index, rhs_index, lhs_sum + rhs_sum}
+    lhs_reversed = Enum.reverse(lhs)
+    {cross_lhs_index, cross_lhs_sum} = find_max_subarray(lhs_reversed, 0, 0, 0)
+    cross_sum = cross_lhs_sum + rhs_sum
+
+    cond do
+      lhs_sum >= rhs_sum and lhs_sum >= cross_sum ->
+        {0, lhs_index, lhs_sum}
+      rhs_sum > lhs_sum and rhs_sum >= cross_sum ->
+        {length(lhs), rhs_index, rhs_sum}
+      true ->
+        {cross_lhs_index, rhs_index, cross_sum}
+    end
   end
 end
--- test/maximum_subarray_test.exs
+++ test/maximum_subarray_test.exs
@@ -1,9 +1,37 @@
 defmodule MergeSortTest do
   use ExUnit.Case
 
-  test "finds maximum subarray of list" do
-    list = [-2, 10, -11, -2]
-    MaximumSubarray.find_max_subarray(list, 0, 0, 0) |> IO.inspect()
-    assert true == true
+  test "find maximum subarray of even sized list" do
+    list = [1, 2, 3, -10, -20, -30]
+    actual = MaximumSubarray.do_search(list)
+    expected = {0, 2, 6}
+    assert actual == expected
+
+    list = [-10, -20, -30, 1, 2, 3]
+    actual = MaximumSubarray.do_search(list)
+    expected = {3, 5, 6}
+    assert actual == expected
+
+    list = [-10, 1, 2, 3, -20, -30]
+    actual = MaximumSubarray.do_search(list)
+    expected = {1, 3, 6}
+    assert actual == expected
+  end
+
+  test "find maximum subarray of odd sized list" do
+    list = [-1, 20, -10, -20, -30]
+    actual = MaximumSubarray.do_search(list)
+    expected = {0, 1, 19}
+    assert actual == expected
+
+    list = [-10, -20, -40, -1, 20]
+    actual = MaximumSubarray.do_search(list)
+    expected = {3, 4, 19}
+    assert actual == expected
+
+    list = [-10, 1, 2, 3, -20]
+    actual = MaximumSubarray.do_search(list)
+    expected = {1, 3, 6}
+    assert actual == expected
   end
 end
